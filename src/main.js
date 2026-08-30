c8cb8eac38666479052ea102c05f27e9cc2dde73
ShrutiPandey-4 <23053394@kiit.ac.in>
2026-08-30 22:56:33 +0530

    Add first 3D star

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './style.css';
 
 const scene = new THREE.Scene();
 
+scene.background = new THREE.Color(0x000000);
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -21,10 +23,35 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 document.body.appendChild(renderer.domElement);
 
+// Star
+const starGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+const starMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffdd66
+});
+
+const star = new THREE.Mesh(
+  starGeometry,
+  starMaterial
+);
+
+scene.add(star);
+
+// Animation
 function animate() {
   requestAnimationFrame(animate);
+
+  star.rotation.y += 0.005;
 
   renderer.render(scene, camera);
 }
 
 animate();
+
+// Responsive screen
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
